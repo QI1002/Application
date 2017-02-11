4456a875bfde50a0581aa9aefd6a2ac14baca8a9
qi1002 <alanchang544@gmail.com>
2017-02-11 16:06:30 +0800

    Add 4 activites for our default learning behaviors

--- app/src/main/java/io/github/qi1002/ilearn/MainActivity.java
+++ app/src/main/java/io/github/qi1002/ilearn/MainActivity.java
@@ -1,13 +1,60 @@
 package io.github.qi1002.ilearn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtLoopupDictionary;
+    private Button mBtPracticeDataset;
+    private Button mBtVocabularyExam;
+    private Button mBtPronunciationExam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtLoopupDictionary = (Button) findViewById(R.id.bt_dictionary_lookup);
+        mBtLoopupDictionary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity(LookupDictionaryActivity.class);
+            }
+        });
+
+        mBtPracticeDataset = (Button) findViewById(R.id.bt_dataset_practice);
+        mBtPracticeDataset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity(PracticeDatasetActivity.class);
+            }
+        });
+
+        mBtVocabularyExam = (Button) findViewById(R.id.bt_exam_vocabulary);
+        mBtVocabularyExam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity(VocabularyExamActivity.class);
+            }
+        });
+
+        mBtPronunciationExam = (Button) findViewById(R.id.bt_exam_pronunciation);
+        mBtPronunciationExam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity(PronunciationExamActivity.class);
+            }
+        });
+    }
+
+    private void launchActivity(Class<?> cls) {
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
     }
 }
+
+

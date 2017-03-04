8d285321cde2fa6cb0356105cfa162baba00e8c2
qi1002 <alanchang544@gmail.com>
2017-03-04 11:54:20 +0800

    add floating button to return back in configuration page

--- app/src/main/java/io/github/qi1002/ilearn/ConfigurationActivity.java
+++ app/src/main/java/io/github/qi1002/ilearn/ConfigurationActivity.java
@@ -1,6 +1,7 @@
 package io.github.qi1002.ilearn;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -49,6 +50,16 @@ public class ConfigurationActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 launchActivity(ConfigurationUtilityActivity.class);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
             }
         });
     }

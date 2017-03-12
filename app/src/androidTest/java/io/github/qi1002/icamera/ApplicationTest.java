404014f56158a87787a536ef9bc401eecf30a7b6
qi1002 <alanchang544@gmail.com>
2017-03-12 12:54:39 +0800

    create iCamera based on google camera2basic github 15b48a8

---
+++ app/src/androidTest/java/io/github/qi1002/icamera/ApplicationTest.java
@@ -0,0 +1,13 @@
+package io.github.qi1002.icamera;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}

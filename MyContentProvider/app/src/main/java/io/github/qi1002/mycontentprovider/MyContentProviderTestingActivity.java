f5b98ec88b52da9d7a6afadf7149f723f7c52692
alanchang <alanchang544@gmail.com>
2019-05-06 16:50:16 +0800

    add the content provider sample with different apks

--- MyContentProvider/app/src/main/java/io/github/qi1002/mycontentprovider/MyContentProviderTestingActivity.java
+++ MyContentProvider/app/src/main/java/io/github/qi1002/mycontentprovider/MyContentProviderTestingActivity.java
@@ -1,4 +1,4 @@
-package io.github.qi1002.mycontentprovidertesting;
+package io.github.qi1002.mycontentprovider;
 
 import android.database.Cursor;
 import android.net.Uri;
@@ -16,7 +16,7 @@ public class MyContentProviderTestingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
         Cursor c = getContentResolver().query(
                 Uri.parse("content://io.github.qi1002.mycontentprovider/MyTable1"),
@@ -28,14 +28,10 @@ public class MyContentProviderTestingActivity extends AppCompatActivity {
                 c.moveToNext();
             }
             c.close();
-        }else {
-            Log.e("CP", "content://io.github.qi1002.mycontentprovider/MyTable1 fail");
-        }
 
-        c = getContentResolver().query(
-                Uri.parse("content://io.github.qi1002.mycontentprovider/MyTable2"),
-                null,null,null,null ); // get all
-        if( c != null ) {
+            c = getContentResolver().query(
+                    Uri.parse("content://io.github.qi1002.mycontentprovider/MyTable2"),
+                    null, null, null, null); // get all
             c.moveToFirst();
             while (c.isAfterLast() == false) {
                 Log.e("CP", "MyTable2 field1:" + c.getString(0) + ", field2:" + c.getString(1));
@@ -43,7 +39,7 @@ public class MyContentProviderTestingActivity extends AppCompatActivity {
             }
             c.close();
         }else {
-            Log.e("CP", "content://io.github.qi1002.mycontentprovider/MyTable2 fail");
+            Log.e("CP", "content://io.github.qi1002.mycontentprovider/MyTable1 fail");
         }
         // Example of a call to a native method
         // TextView tv = (TextView) findViewById(R.id.sample_text);
@@ -54,5 +50,5 @@ public class MyContentProviderTestingActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    //public native String stringFromJNI();
 }
